38ffb3ced84c3c134bc5177d32b834fdbe0182e9
ELGAZAR_ALWAZER❤ <108340300+Sjshskdhd@users.noreply.github.com>
2023-04-24 05:35:53 +0200

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -5,15 +5,15 @@ class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
     APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
 
-    API_HASH = "ضع كود الايبي هاش"
+    API_HASH = "77ce259be087376f09c0ac1932e8c55e"
 
-    ALIVE_NAME = "اسم حسابك التلي"
+    ALIVE_NAME = "◐ᗩᗷᗪᗩᒪᒪᗩᕼ ᗰOᕼᗩᗰᗴᗪ🖤🎶.“⁪⁬⁮⁮⁮⁮ ‌‌‌‌"
 
     DB_URI = "رابـط التخـزين الخـاص بك"
 
-    STRING_SESSION = "كود تيرمــكس"
+    STRING_SESSION = "1BJWap1sBuzSvb0sQ78Dt6aU0WZrKxMNadNRj0NZ3xZTcZDcTSWiNqkCRRiXuprwfHug2wEfz4Km059plKt0D9bMD9HxSohzU96U7-LXAp6rlvJkT6IYMshUcr_MNQRfAH3ZU9HpjqsbTWss8BdAV0b4WykS9FxYYAhojOSlBO_zjL54UfTozvVRAQHTf4spRFnEtDZbhLub3jWdPZSklC-vAl0BaJUKEmmayGIJbJvcu-POHLF22Fai6HfYMkspNLNiMrU8C2ZfNNmx696ymnwk020H4Rr6-j2vFjp47G6rmE1gVEdelQozWvrStJHwBnKvS4CI5SJVbhLYoOmE-hlw1Iwevr1A="
 
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
+    TG_BOT_TOKEN = "6171410939:AAGun1KbgE-fG-7dxDeQboha8BSJjk0XTmc"
 
     PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
 
